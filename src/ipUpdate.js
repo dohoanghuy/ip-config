ae68b418c5a94b0e1575e022a1bd3a644c3a1c81
Do Hoang Huy <dohoanghuy94@gmail.com>
2022-07-03 21:36:51 +0700

    update code

---
+++ src/ipUpdate.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const fs = require('fs');
+const { logger } = require('./util');
+const { execSync } = require('child_process');
+
+const CHECK_INTERVAL_IN_MS = 1 * 3 * 1000;
+
+const commitIpChange = (ip) => {
+    console.log({
+        a: process.cwd(),
+        b: __dirname
+    });
+    execSync(`git add ${__dirname}/config/ip.json`, { cwd: process.cwd() });
+    execSync(`git commit -m "update ip ${ip}"`, { cwd: process.cwd() });
+    execSync(`git push`, { cwd: process.cwd() });
+    // const stdout = execSync('git status', { cwd: process.cwd() }).toString();
+    // console.log(stdout);
+    // execSync('git status', { cwd: __dirname });
+}
+setInterval(async () => {
+    try {
+        const rawdata = fs.readFileSync(`${process.cwd()}/src/config/ip.json`);
+        const ip = JSON.parse(rawdata);
+        const publicIp = await axios.get('https://api.ipify.org?format=json');
+        logger.info({ ip: ip['crypto-web-tool'], publicIp: publicIp.data.ip });
+
+        if (ip['crypto-web-tool'] === publicIp.data.ip) return;
+        logger.info(`${new Date()} need to update ip now!!!`);
+        fs.writeFileSync(`${process.cwd()}/src/config/ip.json`, JSON.stringify({ 'crypto-web-tool': publicIp.data.ip }));
+
+        commitIpChange(publicIp.data.ip);
+    } catch (error) {
+        logger.error('error happen', error);
+    }
+}, CHECK_INTERVAL_IN_MS);
---
+++ src/util.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const moment = require('moment');
+
+const writeAnalyzeLog = (msg) => {
+    const today = moment().format('YYYY-MM-DD');
+    let dir = `${process.cwd()}/log/${process.env.NODE_ENV}/${today}`;
+    if (!fs.existsSync(`${dir}`)) fs.mkdirSync(`${dir}`, { recursive: true });
+
+    const filename = `${dir}/analyze.json`;
+    fs.writeFileSync(filename, JSON.stringify(msg))
+}
+
+const writeLogToFile = (msg, logType, data) => {
+    try {
+        // Console log
+        if (
+            logType === 'info'
+        ) {
+            console.log(msg, data);
+        }
+
+
+        // Write to log file
+        const today = moment().format('YYYY-MM-DD');
+        let dir = `${process.cwd()}/log/ip-config/${process.env.NODE_ENV}/${today}`;
+        if (!fs.existsSync(`${dir}`)) fs.mkdirSync(`${dir}`, { recursive: true });
+
+        let dataStr = data ? JSON.stringify(data) : '';
+        if (logType === 'error') dataStr = data.toString();
+        const line = `${moment().toString()} - ${JSON.stringify(msg)} ${dataStr}`;
+
+        const filename = `${dir}/${logType}.txt`;
+        fs.appendFileSync(filename, `${line}\n`, 'utf-8')
+    } catch (error) {
+        console.log('Error writeLogToFile:', error);
+    }
+}
+
+module.exports = {
+    logger: {
+        info: (msg, data) => writeLogToFile(msg, `info`, data),
+        error: (msg, error) => writeLogToFile(msg, `error`, error),
+        logTx: (msg, data) => writeLogToFile(msg, `tx`, data),
+        logTxEr: (msg, data) => writeLogToFile(msg, `txEr`, data),
+        logLPTx: (msg, data) => writeLogToFile(msg, `LPTx`, data),
+        logBuyTx: (msg, data) => writeLogToFile(msg, `BuyTx`, data),
+        logSellTx: (msg, data) => writeLogToFile(msg, `SellTx`, data),
+        newToken: (msg, data) => writeLogToFile(msg, `newToken`, data),
+        writeAnalyzeLog: (msg) => writeAnalyzeLog(msg),
+    }
+}
