b09b071ee1d74f3db68ef49f5f02e73743e1bbe7
Do Hoang Huy <dohoanghuy94@gmail.com>
2023-10-28 18:42:19 +0700

    bugfix: spammer

--- src/ipUpdate.js
+++ src/ipUpdate.js
@@ -11,7 +11,8 @@ const commitIpChange = (ip) => {
         b: __dirname
     });
     execSync(`git pull`, { cwd: process.cwd() });
-    execSync(`git add ${__dirname}/config/ip.json`, { cwd: process.cwd() });
+    execSync(`git add .`, { cwd: process.cwd() });
+    // execSync(`git add ${__dirname}/config/ip.json`, { cwd: process.cwd() });
     execSync(`git commit -m "update ip ${ip}"`, { cwd: process.cwd() });
     execSync(`git push`, { cwd: process.cwd() });
     // const stdout = execSync('git status', { cwd: process.cwd() }).toString();
@@ -19,7 +20,7 @@ const commitIpChange = (ip) => {
     // execSync('git status', { cwd: __dirname });
 }
 
-const checkAndUpdateIp = async () => {
+const checkAndUpdateIp = async (bot) => {
     try {
         console.log('Start fetch ip ...');
         const rawdata = fs.readFileSync(`${process.cwd()}/src/config/ip.json`);
@@ -35,6 +36,8 @@ const checkAndUpdateIp = async () => {
         logger.info(`${new Date().toISOString()}`, { ip: ip['crypto-web-tool'], publicIp });
 
         if (ip['crypto-web-tool'] === publicIp) return;
+        bot.telegram.sendMessage(1906945459, `New ip: ${publicIp}`, { parse_mode: 'HTML', disable_web_page_preview: 'true' })
+
         console.log(`${new Date()} need to update ip now!!!`);
         logger.info(`${new Date()} need to update ip now!!!`);
         fs.writeFileSync(`${process.cwd()}/src/config/ip.json`, JSON.stringify({ 'crypto-web-tool': publicIp }));
@@ -42,10 +45,27 @@ const checkAndUpdateIp = async () => {
         commitIpChange(publicIp);
     } catch (error) {
         logger.error('error happen', error);
+        bot.telegram.sendMessage(1906945459, `Update ip error ${JSON.stringify(error)}`, { parse_mode: 'HTML', disable_web_page_preview: 'true' })
     }
 }
 
 (async () => {
-    await checkAndUpdateIp();
-    setInterval(checkAndUpdateIp, CHECK_INTERVAL_IN_MS);
+    const bot = new Telegraf("5153851993:AAGglGcrB86Z7w--5pdUNKM3qs_udO8l-II");
+    bot.start(async (ctx) => ctx.reply(`Hello`));
+    bot.catch((error, ctx) => {
+        console.log(`Ooops, encountered an error for ${ctx.updateType}`, error);
+        setTimeout(ctx.telegram.sendMessage(1906945459, `Có lỗi xảy ra. ${JSON.stringify(error)}`), 10000);
+    });
+    bot.launch();
+
+    // Enable graceful stop
+    process.once('SIGINT', () => {
+        bot.stop('SIGINT');
+    });
+    process.once('SIGTERM', () => {
+        bot.stop('SIGTERM')
+    });
+
+    await checkAndUpdateIp(bot);
+    setInterval(() => checkAndUpdateIp(bot), CHECK_INTERVAL_IN_MS);
 })();
