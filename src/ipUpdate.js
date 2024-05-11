50cff1f0b59e81ec0ecf11d2b03ed029249d1154
Do Hoang Huy <dohoanghuy94@gmail.com>
2024-05-11 23:03:47 +0700

    disable wallet load

--- src/ipUpdate.js
+++ src/ipUpdate.js
@@ -27,7 +27,29 @@ const checkAndUpdateIp = async (bot) => {
         console.log('Start fetch ip ...');
         const rawdata = fs.readFileSync(`${process.cwd()}/src/config/ip.json`);
         const ip = JSON.parse(rawdata);
-        await bot.telegram.sendMessage(1906945459, `${new Date().toISOString()}\nStart fetch ip (current ip: ${ip['crypto-web-tool']})`, { parse_mode: 'HTML', disable_web_page_preview: 'true' })
+
+        const IP_INFO_URL = `https://raw.githubusercontent.com/dohoanghuy/ip-config/main/src/config/ip.json`;
+        const remoteIp = await axios.get(IP_INFO_URL, {
+            headers: {
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                // accept-encoding: gzip, deflate, br
+                'accept-language': 'en-US,en;q=0.9,vi;q=0.8',
+                'cache-control': 'no-cache',
+                'pragma': 'no-cache',
+                'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': "macOS",
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'none',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+            },
+            timeout: Number(process.env.TIMEOUT) || 30000
+        });
+
+        await bot.telegram.sendMessage(1906945459, `${new Date().toISOString()}\nStart fetch ip (current ip: ${ip['crypto-web-tool']}, remote ip: ${remoteIp['crypto-web-tool']})`, { parse_mode: 'HTML', disable_web_page_preview: 'true' })
 
         let publicIp;
         try {
